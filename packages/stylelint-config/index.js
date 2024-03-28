233fc84fe8ebdc48e880eee619fcaf6d9f92cda8
sxy15 <sxy_19921115@163.com>
2024-03-29 00:22:47 +0800

    feat: stylelint init stylelint

---
+++ packages/stylelint-config/index.js
@@ -0,0 +1,10 @@
+export default {
+  "extends": ["stylelint-config-standard-scss"],
+  "rules": {
+    "declaration-property-unit-allowed-list": {
+      "/^border/": ["px"],
+    },
+    "color-named": "never",
+    "function-disallowed-list": ["rgb", "hwb", "lch"],
+  }
+}
---
+++ stylelint.config.js
@@ -0,0 +1,3 @@
+export default {
+  "extends": ["@anfu/stylelint-config"],
+}
